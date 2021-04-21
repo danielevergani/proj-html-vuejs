99eb628dc6a9472bcd8f006c4ebcc6c912308b25
danielevergani <dvergani94@gmail.com>
2021-04-21 15:50:42 +0200

    inserita background image a sec testimonial

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      links:["home", "about", "project", "testimonial", "blog", "contact"]
+      links:["home", "about", "project", "testimonial", "blog", "contact"],
+      background:["images/oliver-ragfelt-488196-2.jpg", "images/oliver-ragfelt-488196-unsplash.jpg", "images/12679.jpg",]
     }
   })
