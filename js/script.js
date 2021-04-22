d339f2764920e1d29bf3b61bb1153fd3043152c9
danielevergani <dvergani94@gmail.com>
2021-04-22 11:53:21 +0200

    carusel, inserita funzione previmg

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ var app = new Vue({
       background:["images/oliver-ragfelt-488196-2.jpg", "images/oliver-ragfelt-488196-unsplash.jpg", "images/12679.jpg"],
       images:["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", "images/image_4.jpeg", "images/image_5.jpg", "images/image_6.jpg"],
       imageToEnd:"",
-      indexImage: 0
+      imageToTop:"",
+      lastImgIndex: ""
     },
 
     methods: {
@@ -14,6 +15,12 @@ var app = new Vue({
 
         this.imageToEnd = this.images.splice(0,1);
         this.images.push(this.imageToEnd[0]);
+      },
+
+      prevImg: function() {
+        this.lastImgIndex = (this.images.length - 1);
+        this.imageToTop = this.images.splice(this.lastImgIndex,1);
+        this.images = [...this.imageToTop, ...this.images];
       }
 
     }
