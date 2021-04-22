f5ce21763b9fe14e5b1f959f9315fd73c6f095f5
danielevergani <dvergani94@gmail.com>
2021-04-22 16:01:34 +0200

    in js bg diventa testimonial (oggetto con tutti i dati per la sez)
    sez construita con v for

--- js/script.js
+++ js/script.js
@@ -2,7 +2,27 @@ var app = new Vue({
     el: '#app',
     data: {
       links:["home", "about", "project", "testimonial", "blog", "contact"],
-      background:["images/oliver-ragfelt-488196-2.jpg", "images/oliver-ragfelt-488196-unsplash.jpg", "images/12679.jpg"],
+      testimonials:[
+        {
+          img: "images/oliver-ragfelt-488196-2.jpg",
+          dichiarazione: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi assumenda veniam aperiam aliquam, eum inventore officia est ad laborum? Sapiente ipsam exercitationem velit necessitatibus illo consequuntur consequatur, quo fugiat explicabo.",
+          nome: "Vera Duncan",
+          azienda: "Amazon Inc."
+        },
+        {
+          img: "images/oliver-ragfelt-488196-unsplash.jpg",
+          dichiarazione: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi assumenda veniam aperiam aliquam, eum inventore officia est ad laborum? Sapiente ipsam exercitationem velit necessitatibus illo consequuntur consequatur, quo fugiat explicabo.",
+          nome: "Vera Duncan",
+          azienda: "Amazon Inc."
+        },
+        {
+          img: "images/12679.jpg",
+          dichiarazione: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi assumenda veniam aperiam aliquam, eum inventore officia est ad laborum? Sapiente ipsam exercitationem velit necessitatibus illo consequuntur consequatur, quo fugiat explicabo.",
+          nome: "Vera Duncan",
+          azienda: "Amazon Inc."
+        }
+      ],
+
       images:["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", "images/image_4.jpeg", "images/image_5.jpg", "images/image_6.jpg"],
       imageToEnd:"",
       imageToTop:"",
